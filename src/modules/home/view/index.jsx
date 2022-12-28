73d3109d8e2c8090e7bfb52a8371f23cd74ca6db
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-12-28 20:58:20 -0300

    feat: styled container on home

--- src/modules/home/view/index.jsx
+++ src/modules/home/view/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../../core/components/header'
+import { Container } from './styles'
 
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Container>
+      </Container>
     </div>
   )
 }
---
+++ src/modules/home/view/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: calc(100vw - 800px);
+    padding: 32px 400px;
+    background: #F3F5F7;
+    height: calc(100vh - 220px);
+`
