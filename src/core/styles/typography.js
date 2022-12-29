761168a0d3cbcb380564bad24d9fc5fd39225548
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-12-28 22:25:55 -0300

    refactor: typography styles

--- src/core/styles/typography.js
+++ src/core/styles/typography.js
@@ -4,13 +4,20 @@ export const Text = styled.p`
     font-family: 'Lexend Deca';
     font-style: normal;
     font-weight: 400;
-    font-size: 24px;
-    line-height: 30px;
+    font-size: 18px;
+    line-height: 32px;
     color: ${props => props.color ? props.color : "black"};
+    margin: 0px;
 `;
 
 export const Title = styled(Text)`
     grid-area: title;
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 26px;
+    line-height: 32px;
+    color: ${props => props.color ? props.color : "black"};
 `;
 
 export const Subtitle = styled(Text)`  
