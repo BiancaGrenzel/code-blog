93530482ec874997f8be3bef3a359aacccca86b3
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-12-28 22:27:18 -0300

    refactor: container style

--- src/modules/home/view/styles.js
+++ src/modules/home/view/styles.js
@@ -4,5 +4,10 @@ export const Container = styled.div`
     width: calc(100vw - 800px);
     padding: 32px 400px;
     background: #F3F5F7;
-    height: calc(100vh - 220px);
+    height: calc(100vh - 196px - 64px);
+    max-height: calc(100vh - 196px - 64px);
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
 `
