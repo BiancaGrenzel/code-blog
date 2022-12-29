dcad2b9b01209abdec0bb3ccb56ade5bc14daf79
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-12-28 22:25:34 -0300

    feat: card style component

---
+++ src/core/components/card/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: calc(100% - 78px);
+    padding: 32px 32px;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`
+
+export const Line = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const Icon = styled.img`
+    width: 20px;
+    height: 20px;
+`
