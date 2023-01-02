0b67799fe735e22fb48738b75abd8e4b0f8f6128
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-02 19:13:49 -0300

    refactor: pass children by props on header

--- src/core/components/header/index.jsx
+++ src/core/components/header/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Header } from './styles'
 import { Typography } from '../../styles'
-import Input from '../input'
 
-function Home() {
+function Home({children}) {
   return (
     <Header>
       <Typography.Title color='#fff'>Codelândia</Typography.Title>
       <Typography.Subtitle color='#fff'>blog</Typography.Subtitle>
-      <Input placeholder='Pesquise aqui'/>
+      {children}
     </Header>
   )
 }
