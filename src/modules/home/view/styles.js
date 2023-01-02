acfa667cdde28eeb7fa8efd39921142d313e3e0d
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-02 19:22:31 -0300

    refactor: container and box styles

--- src/modules/home/view/styles.js
+++ src/modules/home/view/styles.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    background-color: #F3F5F7;
+`
+
+export const Box = styled.div`
     width: calc(100vw - 800px);
-    padding: 32px 400px;
+    padding: 0px 400px;
+    margin: 32px 0px 0px 0px;
     background: #F3F5F7;
-    height: calc(100vh - 196px - 64px);
-    max-height: calc(100vh - 196px - 64px);
+    height: calc(100vh - 196px - 32px);
+    max-height: calc(100vh - 196px - 32px);
     overflow-y: scroll;
     display: flex;
     flex-direction: column;
