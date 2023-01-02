b38ca2b6b9d1e2edd4a5077d9c6d9fcaaee7fce1
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-01 22:25:11 -0300

    feat: show request values

--- src/modules/home/view/index.jsx
+++ src/modules/home/view/index.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import Card from '../../../core/components/card'
 import Header from '../../../core/components/header'
 import { Container } from './styles'
-
+import { useState } from 'react'
+import moment from 'moment'
+import { news } from '../models/News'
 
 function Home() {
+  const [newsList, setNewsList] = useState([])
+
+  const getNews = useCallback(async () => {
+    const response = await news.getNewsByPopularity();
+    console.log(response)
+    setNewsList(response.articles);
+  }, [])
+  
+  useEffect(() => {
+    getNews()
+  }, [])
+
+  const renderNews = () => {
+    return newsList?.map((item) => {
+      return (
+        <Card date={moment(item.publishedAt).utc().format('MM/DD/YYYY')} title={item.title} text={item.description} key={`${item.title}-${Math.random()}`}/>
+      )
+    })
+  }
+
   return (
     <div>
       <Header/>
       <Container>
-        <Card/>
+        {renderNews()}
       </Container>
     </div>
   )
