36cf29cde4a4d09160ca36f79351cae488851022
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2023-01-01 22:24:23 -0300

    refactor: use props on card

--- src/core/components/card/index.jsx
+++ src/core/components/card/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Typography } from '../../styles';
 import { Container, Icon, Line} from './style';
 
-const Card = () => {
+const Card = ({date, title, text}) => {
     return (
         <Container>
             <Line>
                 <Typography.Text>
-                    02 de jul, 2021
+                    {date}
                 </Typography.Text>
                 <Icon src='https://www.svgrepo.com/show/147921/heart.svg'/>
             </Line>
             <Typography.Title>
-                Agora é oficial: o Windows 11 está vindo
+                {title}
             </Typography.Title>
             <Typography.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.
+                {text}
             </Typography.Text>
         </Container>
     )
